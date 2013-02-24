2225176b24d6de2a3720ecfcf8b46f57b7bb8d7c
Charles Wong <charleswong@Charless-MacBook-Pro.local>
2013-02-24 03:48:16 -0500

    IS IT WORKING?

---
+++ assignment0/assignment0_question2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# fizzbuzz question: Write a program that prints the numbers from 1 
+# to 100. But for multiples of three print “Fizz” instead of the 
+# number and for the multiples of five print “Buzz”. For numbers 
+# which are multiples of both three and five print “FizzBuzz”.
+
+# so the user can specify the range of numbers the code loops through
+max_num = 30
+
+i = 1
+while i <= 100
+  if i % 15 = 0
+    print "FizzBuzz"
+  elif i % 3 = 0
+    print "Fizz"
+  elif i % 5 = 0
+    print "Buzz"
+  else 
+    print i
